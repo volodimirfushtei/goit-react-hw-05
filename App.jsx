0d36b4297a52fa3d2a894f6c1d4afeef7ac211f5
Volodimir <161057867+volodimirfushtei@users.noreply.github.com>
2024-09-30 05:39:23 +0300

    add

--- App.jsx
+++ App.jsx
@@ -6,11 +6,7 @@ import NotFoundPage from "./src/pages/NotFoundPage/NotFoundPage";
 import MovieDetailsPage from "./src/pages/MovieDetailsPage/MovieDetailsPage";
 import MovieCast from "./src/components/MovieCast/MovieCast";
 import MovieReviews from "./src/components/MovieReviews/MovieReviews";
-// 19ccb5d526bae3cf70acea6c2f2cc1d6
 
-// eyJhbGciOiJIUzI1NiJ9
-//   .eyJhdWQiOiIxOWNjYjVkNTI2YmFlM2NmNzBhY2VhNmMyZjJjYzFkNiIsIm5iZiI6MTcyNzIxMzIxMS4zNTM4MjQsInN1YiI6IjY2ZjMyN2ZjODQxMDk4NzE4NWMxZGZmYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ
-//   .jgNiulurOP8bbMSl1S1bTwt1DPITs9nAXK6ONaJeN1g;
 function App() {
   return (
     <>

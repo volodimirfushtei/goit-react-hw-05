e37bf9b143647508d181ea089517248b62352e62
Volodimir <161057867+volodimirfushtei@users.noreply.github.com>
2024-10-02 00:33:38 +0300

    add

--- src/components/MovieListSearch/MovieListSearch.jsx
+++ src/components/MovieListSearch/MovieListSearch.jsx
@@ -1,14 +1,17 @@
-import { Link } from "react-router-dom";
-
+import { Link, useLocation } from "react-router-dom";
 import s from "./MovieListSearch.module.css";
 
 const MovieListSearch = ({ movies }) => {
+  const location = useLocation(); // Отримуємо поточне місцезнаходження
+
   return (
     <ul className={s.movies_list}>
       {movies.map((movie) => (
         <li className={s.movies_list_item} key={movie.id}>
           <div className={s.create_line}></div>
-          <Link to={`/movies/${movie.id}`}>{movie.title}</Link>
+          <Link to={`/movies/${movie.id}`} state={{ from: location }}>
+            {movie.title}
+          </Link>
         </li>
       ))}
     </ul>
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,9 +1,43 @@
-import MovieList from "../../components/MovieList/MovieList";
+import { useEffect, useState } from "react";
+import MovieListSearch from "../../components/MovieListSearch/MovieListSearch";
 import s from "./HomePage.module.css";
+import Loader from "../../../src/components/Loader/Loader";
+import fetchApi from "../../servises/Api";
 const HomePage = () => {
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const loadMovies = async () => {
+      setLoading(true);
+      try {
+        const data = await fetchApi.fetchMovies();
+        setMovies(data.results);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    loadMovies();
+  }, []);
+
+  if (loading) {
+    return <Loader />;
+  }
+
+  if (error) {
+    return <p>Помилка: {error}</p>;
+  }
+  if (!movies || movies.length === 0) {
+    return <p>Не вдалося завантажити фільми.</p>;
+  }
+
   return (
     <div className={s.MovieList}>
-      <MovieList />
+      <MovieListSearch movies={movies} />
     </div>
   );
 };
